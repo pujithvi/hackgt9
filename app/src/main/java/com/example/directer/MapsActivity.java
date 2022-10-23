285571af51898284490a12b2e555e56ec9cf9366
AceAditya <37184739+AceAditya@users.noreply.github.com>
2022-10-23 01:39:15 -0400

    Fixed current location

--- app/src/main/java/com/example/directer/MapsActivity.java
+++ app/src/main/java/com/example/directer/MapsActivity.java
@@ -1,10 +1,22 @@
 package com.example.directer;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -12,23 +24,63 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.directer.databinding.ActivityMapsBinding;
-import com.google.maps.errors.ApiException;
+import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.io.IOException;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private ActivityMapsBinding binding;
+    private FusedLocationProviderClient fusedLocationClient;
+    public static double longitude;
+    public static double latitude;
 
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            latitude = location.getLatitude();
+            longitude = location.getLongitude();
+            //System.out.println("lat " + latitude + ", long " + longitude);
+        }
+    };
+
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final Location[] current_loc = new Location[1];
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            // ask permissions here using below code
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    102);
+        }
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 15, 0, mLocationListener);
+
+        /*
+        FusedLocationProviderClient fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            current_loc[0] = location;
+                        }
+                    }
+                });
+
+         */
         StrictMode.ThreadPolicy gfgPolicy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(gfgPolicy);
         mapsApi a = new mapsApi();
-        a.onPostExecute();
-        System.out.println("here");
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        latitude = location.getLatitude();
+        longitude = location.getLongitude();
+        Object[] params = {latitude, longitude, 30};
+        a.onPostExecute(params);
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -37,6 +89,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
 
     /**
      * Manipulates the map once available.
--- app/src/main/java/com/example/directer/mapsApi.java
+++ app/src/main/java/com/example/directer/mapsApi.java
@@ -1,38 +1,113 @@
 package com.example.directer;
 
+import static androidx.core.content.ContextCompat.getSystemService;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.maps.DistanceMatrixApiRequest;
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.GeocodingApiRequest;
+import com.google.maps.NearbySearchRequest;
+import com.google.maps.PendingResult;
+import com.google.maps.PlacesApi;
+import com.google.maps.TextSearchRequest;
 import com.google.maps.errors.ApiException;
+import com.google.maps.model.DistanceMatrix;
+import com.google.maps.model.DistanceMatrixElement;
+import com.google.maps.model.DistanceMatrixRow;
 import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.PlaceType;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
+import com.google.maps.model.TravelMode;
+
+import org.w3c.dom.Text;
 
 import java.io.IOException;
+import java.security.KeyException;
+import java.util.*;
+
 
 public class mapsApi extends AsyncTask {
-    public Object doInBackground (Object[] objects) {
+
+    public Object doInBackground (Object[] params) {
         GeoApiContext context = new GeoApiContext.Builder()
                 .apiKey("AIzaSyD0e_AjT0xXkIjPu3VswknGPL62DVSp4oI")
                 .build();
-        GeocodingResult[] results = new GeocodingResult[0];
+        LatLng current_loc = new LatLng(MapsActivity.latitude, MapsActivity.longitude);
+        System.out.println("Lat: " + current_loc.lat + " Long: " + current_loc.lng);
+        Integer radius = ((Integer) params[2]) * 1600;
+        ArrayList<PlacesSearchResult> places = new ArrayList<PlacesSearchResult>();
+        NearbySearchRequest search_results = PlacesApi.nearbySearchQuery(context, current_loc).radius(radius);
+        search_results.type(PlaceType.HOSPITAL);
         try {
-            results = GeocodingApi.geocode(context,
-                    "1600 Amphitheatre Parkway Mountain View, CA 94043").await();
-        } catch (IOException | ApiException | InterruptedException e) {
-            e.printStackTrace();
+            PlacesSearchResponse result = search_results.await();
+            Collections.addAll(places, result.results);
+        } catch (Exception e) {
+            System.out.println("Search Request Failed.");
         }
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-        System.out.println(gson.toJson(results[0].addressComponents));
+        HashMap<String, ArrayList<Integer>> times_map = new HashMap<String, ArrayList<Integer>>();
+        System.out.println("Places: " + places.size());
+        for (PlacesSearchResult p : places) {
+            try{
+                boolean openNow = p.openingHours.openNow;
+                if (!openNow) continue;
+            } catch (NullPointerException e) {
+                System.out.println(e.getMessage());
+            }
+
+            LatLng location = p.geometry.location;
+            DistanceMatrixApiRequest req = new DistanceMatrixApiRequest(context).origins(current_loc).destinations(location).mode(TravelMode.DRIVING);
+            try {
+                DistanceMatrix dist = req.await();
+                for (DistanceMatrixRow row : dist.rows) {
+                    for (DistanceMatrixElement ele : row.elements) {
+                        System.out.println(ele.duration.humanReadable);
+                        int travelTime = Integer.valueOf(ele.duration.humanReadable.split(" ")[0]);
+                        int waitTime = (int) (Math.random() * 25) + 1;
+                        ArrayList<Integer> times = new ArrayList<>();
+                        times.add(travelTime);
+                        times.add(waitTime);
+                        times.add(travelTime + waitTime);
+                        times_map.put(p.name, times);
+                    }
+                }
+                System.out.println("Finished place: " + p.name);
+            } catch (Exception e) {
+                System.out.println(e);
+                System.out.println("Distance Matrix Calculation Failed.");
+            }
+
+
+            //times_map.put(p.name, )
+        }
+        List<Map.Entry<String, ArrayList<Integer>>> list = new LinkedList<>(times_map.entrySet());
+        Collections.sort(list, (l1, l2) -> l1.getValue().get(2).compareTo(l2.getValue().get(2)));
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
 
 // Invoke .shutdown() after your application is done making requests
         context.shutdown();
         return null;
     }
-    public void onPostExecute() {
-        doInBackground(null);
+    public void onPostExecute(Object[] params) {
+        doInBackground(params);
     }
 
 
